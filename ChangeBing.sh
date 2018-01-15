3228fdb8eb18032327badda55c1453a34fd82a84
kissshot <k@03k.org>
2018-01-15 16:34:50 +0800

    解决需要预先设置背景的问题

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,5 +1,6 @@
 save="/volume1/web/bing.jpg"
 wget "https://cn.bing.com/$(curl -s "http://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"|grep -Po 'url[" :]+\K[^"]+')" -qO $save
+sed -i s/login_background_customize=.*/login_background_customize=\"yes\"/g /etc/synoinfo.conf
 ln -sf $save /usr/syno/etc/login_background_hd.jpg
 ln -sf $save /usr/syno/etc/login_background.jpg
 story=$(curl -s https://cn.bing.com/cnhp/coverstory)
@@ -7,3 +8,4 @@ title=$(echo $story|grep -Po 'title[" :]+\K[^"]+')
 attribute=$(echo $story|grep -Po 'attribute[" :]+\K[^"]+')
 sed -i s/login_welcome_title=.*/login_welcome_title=\"$title\"/g /etc/synoinfo.conf
 sed -i s/login_welcome_msg=.*/login_welcome_msg=\"$attribute\"/g /etc/synoinfo.conf
+#ln -sf $save /usr/syno/etc/preference/admin/wallpaper
