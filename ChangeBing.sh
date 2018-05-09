4d6cf4bd9114f9d65ec5f7f6c8f8e941890b61b2
kissshot <k@03k.org>
2018-05-09 15:53:48 +0800

    修正新群晖问题

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,5 +1,12 @@
 save="/volume1/web/bing.jpg"
 wget "https://cn.bing.com/$(curl -s "http://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"|grep -Po 'url[" :]+\K[^"]+')" -qO $save
+if !(cat /etc/synoinfo.conf|grep login_background_customize) then
+echo "login_background_customize=1">>/etc/synoinfo.conf
+fi
+if !(cat /etc/synoinfo.conf|grep login_welcome_title) then
+echo "login_welcome_title=1">>/etc/synoinfo.conf
+echo "login_welcome_msg=1">>/etc/synoinfo.conf
+fi
 sed -i s/login_background_customize=.*/login_background_customize=\"yes\"/g /etc/synoinfo.conf
 ln -sf $save /usr/syno/etc/login_background_hd.jpg
 ln -sf $save /usr/syno/etc/login_background.jpg
