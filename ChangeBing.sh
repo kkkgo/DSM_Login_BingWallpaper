2d995bc97d8ace287eeba7f9313213ba955233e8
kissshot <k@03k.org>
2019-01-11 02:22:44 +0800

    兼容DSM5

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,18 +1,15 @@
-save="/volume1/web/bing.jpg"
-wget "https://cn.bing.com/$(curl -s "http://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"|grep -Po 'url[" :]+\K[^"]+')" -qO $save
-if !(cat /etc/synoinfo.conf|grep login_background_customize) then
-echo "login_background_customize=1">>/etc/synoinfo.conf
-fi
-if !(cat /etc/synoinfo.conf|grep login_welcome_title) then
-echo "login_welcome_title=1">>/etc/synoinfo.conf
-echo "login_welcome_msg=1">>/etc/synoinfo.conf
-fi
-sed -i s/login_background_customize=.*/login_background_customize=\"yes\"/g /etc/synoinfo.conf
+save="/usr/syno/etc/bing.jpg"
+wget "https://bing.com$(curl -s "https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g')" -qO $save
+story=$(curl -s https://www4.bing.com/cnhp/coverstory)
+sed -i s/login_background_customize=.*//g /etc/synoinfo.conf
+echo login_background_customize=\"yes\">>/etc/synoinfo.conf
 ln -sf $save /usr/syno/etc/login_background_hd.jpg
 ln -sf $save /usr/syno/etc/login_background.jpg
-story=$(curl -s https://www4.bing.com/cnhp/coverstory)
-title=$(echo $story|grep -Po 'title[" :]+\K[^"]+')
-attribute=$(echo $story|grep -Po 'attribute[" :]+\K[^"]+')
-sed -i s/login_welcome_title=.*/login_welcome_title=\"$title\"/g /etc/synoinfo.conf
-sed -i s/login_welcome_msg=.*/login_welcome_msg=\"$attribute\"/g /etc/synoinfo.conf
+if (echo $story|grep attribute) then
+sed -i s/login_welcome_title=.*//g /etc/synoinfo.conf
+echo "login_welcome_title=\"$(echo $story|sed 's/.\+"title"[: "]\+//g'|sed 's/",".\+//g')\"">>/etc/synoinfo.conf
+sed -i s/login_welcome_msg=.*//g /etc/synoinfo.conf
+echo "login_welcome_msg=\"$(echo $story|sed 's/.\+"attribute"[: "]\+//g'|sed 's/",".\+//g')\"">>/etc/synoinfo.conf
+fi
+#用户桌面
 #ln -sf $save /usr/syno/etc/preference/admin/wallpaper
