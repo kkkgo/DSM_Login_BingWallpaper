0bd5e248f85d73b6de927cdf15c8935de88294e3
kissshot <4530897+kkkgo@users.noreply.github.com>
2019-01-11 19:30:04 +0800

    保存功能

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,4 +1,9 @@
 save="/usr/syno/etc/bing.jpg"
+#如果需要收集保存每日美图可去掉注释
+#保存路径path，请设置自己的文件夹，在FileStation里面右键文件夹属性可以看到路径
+#path="/volume1/wallpaper/"
+#name="$(date +'%Y%m%d')_bing.jpg"
+#save=$path$name
 wget "https://bing.com$(curl -s "https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g')" -qO $save
 story=$(curl -s https://www4.bing.com/cnhp/coverstory)
 sed -i s/login_background_customize=.*//g /etc/synoinfo.conf
@@ -11,5 +16,5 @@ echo "login_welcome_title=\"$(echo $story|sed 's/.\+"title"[: "]\+//g'|sed 's/",
 sed -i s/login_welcome_msg=.*//g /etc/synoinfo.conf
 echo "login_welcome_msg=\"$(echo $story|sed 's/.\+"attribute"[: "]\+//g'|sed 's/",".\+//g')\"">>/etc/synoinfo.conf
 fi
-#用户桌面
+#设置用户桌面背景
 #ln -sf $save /usr/syno/etc/preference/admin/wallpaper
