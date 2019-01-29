0faaa157cfea7646ffb5170589ff38f634fc7178
kissshot <4530897+kkkgo@users.noreply.github.com>
2019-01-29 18:29:17 +0800

    修复bug和改进

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,20 +1,33 @@
-save="/usr/syno/etc/bing.jpg"
-#如果需要收集保存每日美图可去掉注释
-#保存路径path，请设置自己的文件夹，在FileStation里面右键文件夹属性可以看到路径
-#path="/volume1/wallpaper/"
-#name="$(date +'%Y%m%d')_bing.jpg"
-#save=$path$name
-wget "https://bing.com$(curl -s "https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g')" -qO $save
-story=$(curl -s https://www4.bing.com/cnhp/coverstory)
-sed -i s/login_background_customize=.*//g /etc/synoinfo.conf
-echo login_background_customize=\"yes\">>/etc/synoinfo.conf
-ln -sf $save /usr/syno/etc/login_background_hd.jpg
-ln -sf $save /usr/syno/etc/login_background.jpg
-if (echo $story|grep attribute) then
-sed -i s/login_welcome_title=.*//g /etc/synoinfo.conf
-echo "login_welcome_title=\"$(echo $story|sed 's/.\+"title"[: "]\+//g'|sed 's/",".\+//g')\"">>/etc/synoinfo.conf
-sed -i s/login_welcome_msg=.*//g /etc/synoinfo.conf
-echo "login_welcome_msg=\"$(echo $story|sed 's/.\+"attribute"[: "]\+//g'|sed 's/",".\+//g')\"">>/etc/synoinfo.conf
+#如需收集每日美图去掉下面注释设置保存文件夹路径
+#savepath="/volume1/wallpaper"
+#在FileStation里面右键文件夹属性可以看到路径
+pic=$(wget -t 5 --no-check-certificate -qO- "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
+if (echo $pic|grep enddate &>/dev/null) then
+link=$(echo https://www.bing.com$(echo $pic|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g'))
+date=$(echo $pic|sed 's/.\+enddate[": ]\+//g'|grep -Eo 2[0-9]{7}|head -1)
+tmpfile=/tmp/$date"_bing.jpg"
+wget -t 5 --no-check-certificate  $link -qO $tmpfile
+rm -rf /usr/syno/etc/login_background*.jpg
+cp -f $tmpfile /usr/syno/etc/login_background.jpg
+cp -f $tmpfile /usr/syno/etc/login_background_hd.jpg
+cp -f $tmpfile /usr/syno/synoman/webman/resources/images/default_wallpaper/01.jpg
+cp -f $tmpfile /usr/syno/synoman/webman/resources/images/default/1x/default_wallpaper/dsm6_01.jpg
+cp -f $tmpfile /usr/syno/synoman/webman/resources/images/default/2x/default_wallpaper/dsm6_01.jpg
+cp -f $tmpfile /usr/syno/synoman/webman/resources/images/default/1x/default_wallpaper/dsm6_02.jpg
+cp -f $tmpfile /usr/syno/synoman/webman/resources/images/default/2x/default_wallpaper/dsm6_02.jpg
+story=$(wget -t 5 --no-check-certificate -qO- "https://www.bing.com/cnhp/life?mkt=zh-CN")
+if (echo $story|grep hplaTtl &>/dev/null) then
+title=$(echo $story|sed 's/.\+"hplaTtl">//g'|sed 's/<.\+//g')
+word=$(echo $story|sed 's/.\+"hplaAttr">//g'|sed 's/<.\+//g')
+if (echo $savepath|grep /&>/dev/null) then
+cp -f $tmpfile $savepath/$date@$title-$word.jpg
 fi
-#设置用户桌面背景
-#ln -sf $save /usr/syno/etc/preference/admin/wallpaper
+sed -i s/login_background_customize=.*//g /etc/synoinfo.conf
+echo "login_background_customize=\"yes\"">>/etc/synoinfo.conf
+sed -i s/login_welcome_title=.*//g /etc/synoinfo.conf
+echo "login_welcome_title=\"$title\"">>/etc/synoinfo.conf
+sed -i s/login_welcome_msg=.*//g /etc/synoinfo.conf
+echo "login_welcome_msg=\"$word\"">>/etc/synoinfo.conf
+fi
+fi
+rm -rf /tmp/*_bing.jpg
