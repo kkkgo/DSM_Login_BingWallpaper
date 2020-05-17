47df1315eeca7205a76fc10091ee27b3ff5c46a6
Robin Nathan <yhz-163@163.com>
2020-05-17 12:41:07 +0800

    Add user wallpaper change optional features
    Add user wallpaper change optional features

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,6 +1,7 @@
-#如需收集每日美图去掉下面注释设置保存文件夹路径
+#如需收集每日美图去掉下面注释设置保存文件夹路径，在FileStation里面右键文件夹属性可以看到路径
 #savepath="/volume1/wallpaper"
-#在FileStation里面右键文件夹属性可以看到路径
+#如需将收集的图片同时应用于用户桌面，请注释掉下面的用户名变量并改为对应用户名，并在“DSM桌面右上角-个人设置-桌面”启用“自定义壁纸”后任意选择一张图片
+#username="admin"
 pic=$(wget -t 5 --no-check-certificate -qO- "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
 echo $pic|grep -q enddate||exit
 link=$(echo https://www.bing.com$(echo $pic|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g'))
@@ -27,5 +28,9 @@ sed -i s/login_welcome_msg=.*//g /etc/synoinfo.conf
 echo "login_welcome_msg=\"$word\"">>/etc/synoinfo.conf
 if (echo $savepath|grep -q '/') then
 cp -f $tmpfile $savepath/$date@$title-$word.jpg
+fi
+if (! -n "$username") then
+cp -f $tmpfile /usr/syno/etc/preference/$username/wallpaper
+chown $user:users /usr/syno/etc/preference/$username/wallpaper
 fi
 rm -rf /tmp/*_bing.jpg
