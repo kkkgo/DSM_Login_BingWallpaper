d96623cf8d9db6629341e68a938881c5847aa109
kkkgo <4530897+kkkgo@users.noreply.github.com>
2022-08-23 22:28:00 +0800

    Fix compatibility and fault tolerance, increase UHD resolution

--- ChangeBing.sh
+++ ChangeBing.sh
@@ -1,31 +1,73 @@
-#如需收集每日美图去掉下面注释设置保存文件夹路径
-#savepath="/volume1/wallpaper"
-#在FileStation里面右键文件夹属性可以看到路径
-pic=$(wget -t 5 --no-check-certificate -qO- "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
-echo $pic|grep -q enddate||exit
+# 如需收集保存壁纸,请去掉下面注释,设置保存文件夹路径
+# 在FileStation里面右键文件夹属性可以看到路径
+# If you want to collect and save Wallpapers, 
+# please remove the comments below and set the savepath.
+# Right click the folder property in FileStation to see the path.
+
+#savepath="/volume1/myshare/wallpaper"
+
+# 如需下载4k分辨率,请设置res=4k
+# 如需下载体积更大的4k以上分辨率的原始图片,请设置res=raw
+# To download 4K resolution, set res = 4K
+# To download a larger original picture, set res = raw
+
+#res=4k
+
+echo "[x]Collecting information..."
+pic="https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+if [ "$res" != "" ]
+then pic="https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&uhd=1&uhdwidth=3840&uhdheight=2160"
+fi
+pic=$(wget -t 5 --no-check-certificate -qO- $pic)
+echo $pic|grep -q startdate||exit
 link=$(echo https://www.bing.com$(echo $pic|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g'))
-date=$(echo $pic|sed 's/.\+enddate[": ]\+//g'|grep -Eo 2[0-9]{7}|head -1)
-tmpfile=/tmp/$date"_bing.jpg"
-wget -t 5 --no-check-certificate  $link -qO $tmpfile
-[ -s $tmpfile ]||exit
-rm -rf /usr/syno/etc/login_background*.jpg
-cp -f $tmpfile /usr/syno/etc/login_background.jpg &>/dev/null
-cp -f $tmpfile /usr/syno/etc/login_background_hd.jpg &>/dev/null
+if [ "$res" == "raw" ]
+then link=$(echo $link|grep -Eo "https://[-=?/._a-zA-Z0-9]+")
+fi
+date=$(echo $pic|grep -Eo '"startdate":"[0-9]+'|grep -Eo '[0-9]+'|head -1)
+if [ "$date" == "" ]
+then date=$(date +%Y%m%d)
+fi
 title=$(echo $pic|sed 's/.\+"title":"//g'|sed 's/".\+//g')
 copyright=$(echo $pic|sed 's/.\+"copyright[:" ]\+//g'|sed 's/".\+//g')
-word=$(echo $copyright|sed 's/(.\+//g')
-if  [ ! -n "$title" ];then
-cninfo=$(echo $copyright|sed 's/，/"/g'|sed 's/,/"/g'|sed 's/(/"/g'|sed 's/ //g'|sed 's/\//_/g'|sed 's/)//g')
-title=$(echo $cninfo|cut -d'"' -f1)
-word=$(echo $cninfo|cut -d'"' -f2)
+keyword=$(echo $copyright|sed 's/, /-/g'|cut -d" " -f1|grep -Eo '[^()\\/:*?"<>]+'|head -1)
+filename="bing_"$date"_"$keyword".jpg"
+echo "Link:"$link
+echo "Date:"$date
+echo "Title:"$title
+echo "Copyright:"$copyright
+echo "Keyword:"$keyword
+echo "Filename:"$filename
+
+echo "[x]Downloading wallpaper..."
+tmpfile=/tmp/$filename
+wget -t 5 --no-check-certificate  $link -qO $tmpfile
+ls -lah $tmpfile||exit
+
+echo "[x]Copying wallpaper..."
+if [ "$savepath" != "" ]
+then cp $tmpfile $savepath
+echo "Save:"$savepath
+ls -lah $savepath|grep $date
+cd $savepath
+chmod 777 $filename
+else echo "savepath is not set, skip copy."
 fi
-sed -i s/login_background_customize=.*//g /etc/synoinfo.conf
-echo "login_background_customize=\"yes\"">>/etc/synoinfo.conf
+
+echo "[x]Setting welcome msg..."
 sed -i s/login_welcome_title=.*//g /etc/synoinfo.conf
 echo "login_welcome_title=\"$title\"">>/etc/synoinfo.conf
 sed -i s/login_welcome_msg=.*//g /etc/synoinfo.conf
-echo "login_welcome_msg=\"$word\"">>/etc/synoinfo.conf
-if (echo $savepath|grep -q '/') then
-cp -f $tmpfile $savepath/$date@$title-$word.jpg
-fi
-rm -rf /tmp/*_bing.jpg
+echo "login_welcome_msg=\"$copyright\"">>/etc/synoinfo.conf
+
+echo "[x]Applying login wallpaper..."
+sed -i s/login_background_customize=.*//g /etc/synoinfo.conf
+echo "login_background_customize=\"yes\"">>/etc/synoinfo.conf
+sed -i s/login_background_type=.*//g /etc/synoinfo.conf
+echo "login_background_type=\"fromDS\"">>/etc/synoinfo.conf
+rm -rf /usr/syno/etc/login_background*.jpg
+cp -f $tmpfile /usr/syno/etc/login_background.jpg
+ln -sf /usr/syno/etc/login_background.jpg /usr/syno/etc/login_background_hd.jpg
+
+echo "[x]Clean..."
+rm -f $tmpfile
--- README.md
+++ README.md
@@ -1,15 +1,24 @@
-# 使用脚本前请先设置任意自带壁纸为背景  
-![sel.png](https://i.loli.net/2020/02/20/Kbn9YQOmsDt6rHd.png)  
-  
 # DSM_Login_BingWallpaper
-把脚本加入群晖DSM的计划任务（请选择root用户）里面即可自动更换登录界面的背景为bing今日美图，并且替换欢迎信息为美图简介.  
-![计划任务设置](https://i.loli.net/2019/01/11/5c378d53206a0.png)
-![登陆界面](https://i.loli.net/2019/01/11/5c378ea0ed2ef.png)
-![今日美图](https://i.loli.net/2019/01/11/5c378ea22efb1.png)
-测试群晖版本DSM5.x,DSM6.x.  
-如果想顺便保存收集bing的每日美图，把第一行的savepath去掉注释填上你自己的要保存的文件夹路径即可。  
-文件夹路径可以在在FileStation里面右键文件夹属性可以看到路径。   
-![修改注释](https://i.loli.net/2019/01/29/5c50302b6fcf8.png)  
-![保存今日美图](https://i.loli.net/2019/01/29/5c502f240d50a.png)    
+把脚本加入群晖DSM的计划任务（请选择root用户）里面即可自动更换登录界面的背景为bing今日美图，并且替换欢迎信息为美图简介.   
+Add the script to the scheduled task of Synology DSM (please select the root user) to automatically change the background of the login interface to Bing Today's wallpaper, and replace the welcome message with wallpaper's introduction.
+![scheduled](https://i.loli.net/2019/01/11/5c378d53206a0.png)
+![login](https://s2.loli.net/2022/08/23/AGm1wq3lUskbYTL.png)
+Test for DSM5.x,DSM6.x. DSM 7.x 
+```sh
+# 如需收集保存壁纸,请去掉下面注释,设置保存文件夹路径
+# 在FileStation里面右键文件夹属性可以看到路径
+# If you want to collect and save Wallpapers, 
+# please remove the comments below and set the savepath.
+# Right click the folder property in FileStation to see the path.
 
-更多信息：https://03k.org/dsm-bing.html    
+#savepath="/volume1/myshare/wallpaper"
+
+# 如需下载4k分辨率,请设置res=4k
+# 如需下载体积更大的4k以上分辨率的原始图片,请设置res=raw
+# To download 4K resolution, set res = 4K
+# To download a larger original picture, set res = raw
+
+#res=4k   
+```
+![save](https://s2.loli.net/2022/08/23/YRUFpG4kK1iV7dy.png)
+More info ：https://03k.org/dsm-bing.html    
